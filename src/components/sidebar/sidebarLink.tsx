eff22421f8a83858cf92538b6cadc53afb0ef0f5
Nima Mehanian <nima.mehanian@gmail.com>
2021-11-09 15:42:43 -0600

    Begin SidebarLink component

---
+++ src/components/sidebar/sidebarLink.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+// import { $ANIMATION_PHYSICS } from '@styles/constants';
+
+function SidebarLink({
+
+}) {
+  return (
+
+  );
+}
+
+export default SidebarLink;
